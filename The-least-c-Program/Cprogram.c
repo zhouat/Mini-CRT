73f7eae365dcc278ed844991c48e1b94388b0503
zhouat <zhouat@mail.ustc.edu.cn>
2014-09-26 22:10:02 +0800

    Hello,world!

---
+++ The-least-c-Program/Cprogram.c
@@ -0,0 +1,29 @@
+char *str="Hello world\n";
+void Print()
+{
+	asm(
+	"movl  $13,%%edx  \n\t"
+	"movl  %0, %%ecx  \n\t"
+	"movl  $0, %%ebx  \n\t"
+	"movl  $4, %%eax  \n\t"
+	"int $0x80        \n\t"
+	::"r"(str):"edx","ecx","ebx"
+	);
+	
+}
+
+void exit()
+{
+	asm(
+	"movl  $42, %ebx  \n\t"
+	"movl  $1,  %eax   \n\t"
+	"int $0x80        \n\t"
+	);
+}
+
+void nomain()
+{
+	Print();
+	exit();
+}
+
