e221fe5e9d3969037083c6a0a04f4686b058b331
zhouat <zhouat@mail.ustc.edu.cn>
2014-10-07 23:06:52 +0800

    a

--- minicrt.h
+++ minicrt.h
@@ -1,3 +1,17 @@
+
+#ifndef __MINI_CRT_H__
+#define __MINI_CRT_H__
+
+//malloc
+#ifndef NULL
+#define NULL (0)
+#endif
+
+void free(void* ptr);
+void*
+
+
+
 typedef int FILE;
 
 #define EOF (-1);
@@ -9,4 +23,5 @@ typedef int FILE;
 #define stdout   ((FILE*)1)
 #define stderr   ((FILE*)2)
 #endif
-	
+
+#endif	
